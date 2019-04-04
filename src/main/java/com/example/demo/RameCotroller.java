efeb28d86d80962c7e7496dbb56e1059e4f49fdc
NAPR\mjaparidze <mjaparidze@napr.local>
2019-04-04 16:20:03 +0400

    first page

--- src/main/java/com/example/demo/RameCotroller.java
+++ src/main/java/com/example/demo/RameCotroller.java
@@ -2,10 +2,13 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class RameCotroller {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String hello(){
         return "heloo!";
     }
